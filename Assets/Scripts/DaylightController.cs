3c4a0bddbec7608c646b3158e3a41b0b2a414001
Sara Makawi <makawi@usc.edu>
2022-02-20 10:11:53 -0800

    daylight + orb

---
+++ Assets/Scripts/DaylightController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daylight : MonoBehavior 
+{
+    public float StartingDaylight = 100f;
+    private const float Scalar = 0.2f;
+    private Hours = StartingDaylight;
+
+    public float DaylightHours 
+    {
+        get {
+            return Hours;
+        } 
+        set {
+            Hours = Mathf.Clamp(value, 0f, StartingDaylight);
+
+            if (Hours <= 0f) {
+                // destroy object idk
+            }
+        }
+    }
+
+    private float Hours = 100f;
+
+    void Start() 
+    {
+        Hours = StartingDaylight;
+    }
+
+    void Update()
+    {
+        Hours -= Scalar * Time.deltaTime;
+        // then change image somehow idk
+    }
+}
---
+++ Assets/Scripts/OrbCollector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehavior 
+{
+    public float Scalar = 0.2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Daylight D = other.GetComponent<Daylight>();
+        destroy(other);
+
+        if (D == null) 
+        {
+            return;
+        }
+
+        else 
+        {
+            D.Hours += 0.2f * Time.deltaTime;
+        }
+    }
+}
